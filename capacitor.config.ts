c6c49a965e9a30c2ab4e8bb3ce5cbeef7b246dd2
ismaelparapi40 <ismaelparapi40@gmail.com>
2025-03-06 02:14:39 +0000

    commit app-tesis-v49.1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'app-tesis',
-  webDir: 'www'
+  webDir: 'dist/app-tesis/browser'
 };
 
 export default config;
